c491bd8fc62e4757e41b189542cf81ddd77e9399
Bhaskar Kumar <bhaskarkr0811@gmail.com>
2023-12-07 18:08:35 -0700

    add : Client Resource : get and getAll

---
+++ src/main/java/com/example/JobSchedulerApplication.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import com.example.resources.ClientResources;
+import com.google.inject.Stage;
+import io.dropwizard.core.Application;
+import io.dropwizard.core.setup.Bootstrap;
+import io.dropwizard.core.setup.Environment;
+import ru.vyarus.dropwizard.guice.GuiceBundle;
+
+public class JobSchedulerApplication extends Application<JobSchedulerConfiguration> {
+
+    private GuiceBundle guiceBundle;
+
+    public static void main(final String[] args) throws Exception {
+        new JobSchedulerApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "jobscheduler";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<JobSchedulerConfiguration> bootstrap) {
+        this.guiceBundle = GuiceBundle.builder()
+                .enableAutoConfig(getClass().getPackage().getName())
+                .build(Stage.PRODUCTION);
+        bootstrap.addBundle(guiceBundle);
+    }
+
+    @Override
+    public void run(final JobSchedulerConfiguration configuration,
+                    final Environment environment) {
+        var injector = guiceBundle.getInjector();
+        environment.jersey().register(injector.getInstance(ClientResources.class));
+    }
+
+}
---
+++ src/main/java/com/example/resources/ClientResources.java
@@ -0,0 +1,31 @@
+package com.example.resources;
+
+import com.google.common.collect.ImmutableList;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+/**
+ *  Author : Bhaskar Kumar
+ *  Date : 7 Dec 2023
+ */
+@Path("/v1/clients")
+@Produces(MediaType.APPLICATION_JSON)
+public class ClientResources {
+
+    @GET
+    public List<String> getAll() {
+        return ImmutableList.of("Client 1");
+    }
+
+    @GET
+    @Path("/{clientId}")
+    public String get(@NotBlank @PathParam("clientId") String clientId) {
+        return clientId;
+    }
+}
